eccee415ebfae11fea7bba92cf2be7a00a9aec22
TrevorRezac <trev.rezac@gmail.com>
2022-05-03 16:24:31 -0700

    passing TDD using initialEntries for CharacterDetail page

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,7 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { MemoryRouter } from 'react-router-dom';
+import { MemoryRouter, Route } from 'react-router-dom';
 import App from './App'
+import CharacterDetail from './views/CharacterDetail';
 describe('App tests', () => {
   it('should render a list of characters. upon clicking character links, user should see an image of the character', async () => {
     render(
@@ -22,8 +23,23 @@ describe('App tests', () => {
 
     screen.debug()
     const link = await screen.findByText('Rick Sanchez');
-    
+
     userEvent.click(link);
+
+    await screen.findByAltText('image of Rick Sanchez')
+  })
+
+  it('should use initial entries to load a character detail page', async () => {
+    render(
+      <MemoryRouter initialEntries={['/characters/1']}>
+        <Route exact path='/characters/:id'>
+          <CharacterDetail />
+      </Route>
+      </MemoryRouter>
+      )
+    screen.getByText(/loading/i)
+
+    await screen.findByText('Rick Sanchez')
 
     await screen.findByAltText('image of Rick Sanchez')
   })
